3eff9b901c0158d9f15e4b9d2cb1f8510878fe80
Viktor Ketipov <viktor@kipiinteractive.com>
2020-01-03 13:26:14 +0200

    Livecoding AI from lecture on 13.12.2019

---
+++ Source/TopDownARPG/BTTask_ActivateSkill.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_ActivateSkill.h"
+#include "EnemyAIController.h"
+
+EBTNodeResult::Type UBTTask_ActivateSkill::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	AEnemyAIController* EnemyAIController = Cast<AEnemyAIController>(OwnerComp.GetOwner());
+	if (IsValid(EnemyAIController))
+	{
+		EnemyAIController->ActivateAbility();
+		return EBTNodeResult::Succeeded;
+	}
+	return EBTNodeResult::Failed;
+}
---
+++ Source/TopDownARPG/BTTask_ActivateSkill.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_ActivateSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNARPG_API UBTTask_ActivateSkill : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
